778f1ab87660498f76a86eeb1c10fb76e3660b59
headlulu888 <headlulu888@gmail.com>
2019-05-28 23:52:55 +0300

    service && factory

--- script.js
+++ script.js
@@ -3,27 +3,50 @@
 (function() {
     var app = angular.module('MyApp', []);
 
-    app.controller('MyController1', [function() {
-        this.name = 'Vasya-111';
+    app.controller('MainController', ['$scope', '$log', 'MyFactory', function($scope, $log, MyFactory) {
+        $scope.books = MyFactory.books;
     }]);
 
-    app.controller('MyController2', [function() {
-        this.name = 'Vasya-222';
+    app.controller('BooksController', ['$scope', '$log', 'MyFactory', function($scope, $log, MyFactory) {
+        $scope.addBook = function() {
+            if (angular.isDefined($scope.name)) {
+                $scope.books.push({
+                    title: $scope.name,
+                    year: 'xxxx',
+                    price: 'XX'
+                });
+            } else {
+                alert('Error!');
+            }
+            $scope.name = '';
+        };
+        $scope.books = MyFactory.books;
+        // $scope.books = [
+        //     { title: 'Book1', year: 1991, price: 100 },
+        //     { title: 'Book2', year: 1992, price: 200 },
+        //     { title: 'Book3', year: 1993, price: 300 },
+        //     { title: 'Book4', year: 1994, price: 400 },
+        //     { title: 'Book5', year: 1995, price: 500 },
+        // ];
     }]);
 
-    app.controller('MyController3', [function() {
-        this.name = '';
-        this.hello = function() {
-            alert('Hello ' + this.name + '!');
-            this.name = '';
-        }
+    app.factory('MyFactory', [function() {
+        // return [
+        //         { title: 'Book1', year: 1991, price: 100 },
+        //         { title: 'Book2', year: 1992, price: 200 },
+        //         { title: 'Book3', year: 1993, price: 300 },
+        //         { title: 'Book4', year: 1994, price: 400 },
+        //         { title: 'Book5', year: 1995, price: 500 },
+        //     ];
 
-        this.books = [
-            { title: 'Book1', year: 1991, price: 100 },
-            { title: 'Book2', year: 1992, price: 200 },
-            { title: 'Book3', year: 1993, price: 300 },
-            { title: 'Book4', year: 1994, price: 400 },
-            { title: 'Book5', year: 1995, price: 500 },
-        ];
+        return {
+            books: [
+                { title: 'Book1', year: 1991, price: 100 },
+                { title: 'Book2', year: 1992, price: 200 },
+                { title: 'Book3', year: 1993, price: 300 },
+                { title: 'Book4', year: 1994, price: 400 },
+                { title: 'Book5', year: 1995, price: 500 },
+            ]
+        };
     }]);
 })();
