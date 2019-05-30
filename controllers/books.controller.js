1faf4f25dfaf24a421a124842bdf6a29b67ee625
headlulu888 <headlulu888@gmail.com>
2019-05-30 16:51:48 +0300

    service

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -3,7 +3,7 @@
 (function() {
     var app = angular.module('MyApp');
     
-    app.controller('BooksController', ['$scope', '$log', 'MyFactory', function($scope, $log, MyFactory) {
+    app.controller('BooksController', ['$scope', '$log', 'MyFactory', '$http', function($scope, $log, MyFactory, $http) {
         $scope.addBook = function() {
             if (angular.isDefined($scope.name)) {
                 $scope.books.push({
@@ -18,6 +18,20 @@
         };
         $scope.books = MyFactory.books;
         $scope.test = 'TEST';
+
+        $scope.test2 = 'tetrr';
+
+        var data = {
+            var1: 'tesr1',
+            var2: 'tesr2'
+        };
+
+        $http.post('data.php', data)
+            .then(function successCallback(response) {
+                $scope.data = response.data;
+            }, function errorCallback(response) {
+                alert('Error!');
+            });
     }]);
 
     app.filter('myFilter', function() {
---
+++ data.php
@@ -0,0 +1,9 @@
+<?php
+
+$answer = [
+    'key1' => 'value 1',
+    'key2' => 'value 2'
+];
+
+$answer = json_encode($answer);
+echo $answer;
