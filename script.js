287c7a383f92663108182474e49fb5f2bf0c1829
headlulu888 <headlulu888@gmail.com>
2019-05-31 13:07:20 +0300

    add animation

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
 (function() {
-    var app = angular.module('MyApp', ['ui.router']);
+    var app = angular.module('MyApp', ['ui.router', 'ngAnimate']);
 
     app.config(['$stateProvider', '$locationProvider', '$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
 			$locationProvider.html5Mode(true);
-			$urlRouterProvider.otherwise('/404');
+			$urlRouterProvider.otherwise('/');
 
 			$stateProvider
 				.state('home', {
@@ -17,24 +17,6 @@
 					url: '/book/:id',
 					controller: 'BookController',
 					templateUrl: 'templates/book.html'
-				})
-				.state('404', {
-					url: '/404',
-					templateUrl: 'templates/404.html'
 				});
-
-			// $routeProvider
-			// 	.when('/', {
-			// 		controller: 'BooksController',
-			// 		templateUrl: 'templates/books.html'
-			// 	})
-			// 	.when('/book/:id', {
-			// 		controller: 'BookController',
-			// 		templateUrl: 'templates/book.html'
-			// 	})
-			// 	.when('/404', {
-			// 		templateUrl: 'templates/404.html'
-			// 	})
-			// 	.otherwise({redirectTo: '/404'});
 		}]);
 })();
