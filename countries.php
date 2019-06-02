54a9db7429899c31259cda13370dd08af6362d0b
headlulu888 <headlulu888@gmail.com>
2019-06-02 23:03:47 +0300

    use filter orderBy

--- countries.php
+++ countries.php
@@ -4,4 +4,5 @@ require 'db.php';
 
 $res = mysqli_query($db, "SELECT country.Code, country.Name, country.Population, city.Name as Capital FROM country LEFT JOIN city ON country.Capital = city.ID ORDER BY country.Code ASC");
 $countries = mysqli_fetch_all($res, MYSQLI_ASSOC);
-exit(json_encode($countries));
+
+exit(json_encode($countries, JSON_NUMERIC_CHECK));
--- js/app.js
+++ js/app.js
@@ -4,10 +4,16 @@
     var app = angular.module('World', []);
 
     // Controllers
-    app.controller('CountriesCtrl', ['$scope', '$http', function($scope, $http) {
+    app.controller('CountriesCtrl', ['$scope', '$http', 'orderByFilter', function($scope, $http, orderBy) {
         $http.post('countries.php')
             .then(function success(response) {
                 $scope.countries = response.data;
+                $scope.order = 'Code';
+                $scope.reverse = false;
+                // $scope.countries = orderBy(response.data, '-Population');
+                angular.forEach($scope.countries, function(country) {
+                    country.Population = parseInt(country.Population);
+                });
             });
     }]);
 })();
