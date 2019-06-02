8d6097e5a75c5df28136bf505748d7e3319eb842
headlulu888 <headlulu888@gmail.com>
2019-06-02 23:29:01 +0300

    pagination 1

--- js/app.js
+++ js/app.js
@@ -11,6 +11,22 @@
                 $scope.order = 'Code';
                 $scope.reverse = false;
                 // $scope.countries = orderBy(response.data, '-Population');
+
+                $scope.page = 1; // текущая страница
+                $scope.perPage = 20;
+                $scope.countPages = Math.ceil($scope.countries.length / $scope.perPage);
+                
+                // Номер предыдущей страницы
+                $scope.prevPage = function() {
+                    return $scope.page--;
+                };
+
+                // Номер следующей страницы
+                $scope.nextPage = function() {
+                    return $scope.page++;
+                };
+
+                // console.log($scope.page);
                 angular.forEach($scope.countries, function(country) {
                     country.Population = parseInt(country.Population);
                 });
