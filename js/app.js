ff4d9bbe0f17ac70c5b449af4145667153ef1afe
headlulu888 <headlulu888@gmail.com>
2019-06-03 15:52:24 +0300

    add new pagination

--- js/app.js
+++ js/app.js
@@ -1,60 +1,48 @@
 'use strict';
 
 (function () {
-    var app = angular.module('World', ['ngAnimate']);
+	var app = angular.module('World', ['ngAnimate']);
+	
+	// Pagination factory
+	app.factory('pagination', function() {
+		var currentPage = 1,
+			itemsPerPage = 10,
+			items = [];
+		return {
+			// Устанавливаем конфигурацию пагинации
+			setItems: function(data, perPage) {
+				items = data;
+				itemsPerPage = angular.isUndefined(perPage) ? itemsPerPage : perPage;
+			},
+			// Кол-во страниц
+			getCountPages: function() {
+				return Math.ceil(items.length / itemsPerPage);
+			},
+			getPaginationList: function () {
+				var countPages = this.getCountPages(),
+					pagination = [];
+
+				if (countPages > 1) {
+					for(var i = 1; i <= countPages; i++) {
+						pagination.push(i);
+					}
+				}
+
+				return pagination;
+			},
+		};
+	});
 
     // Controllers
-    app.controller('CountriesCtrl', ['$scope', '$http', 'orderByFilter', function($scope, $http, orderBy) {
+    app.controller('CountriesCtrl', ['$scope', '$http', 'orderByFilter', 'pagination', function($scope, $http, orderBy, pagination) {
         $http.post('countries.php')
             .then(function success(response) {
                 $scope.countries = response.data;
                 $scope.order = 'Code';
                 $scope.reverse = false;
-                // $scope.countries = orderBy(response.data, '-Population');
+				$scope.perPage = 20;
 
-
-				// $scope.page = 1; // текущая страница
-				if (localStorage.getItem('page')) {
-					$scope.page = +localStorage.getItem('page');
-				} else {
-					$scope.page = 1;
-					localStorage.setItem('page', 1);
-				}
-                $scope.perPage = 10;
-                $scope.countPages = Math.ceil($scope.countries.length / $scope.perPage);
-                
-                // номер предыдущей страницы
-				$scope.prevPage = function(){
-					$scope.page -= 1;
-					localStorage.setItem('page', $scope.page);
-					return $scope.page;
-				};
-
-				// номер следующей страницы
-				$scope.nextPage = function(){
-					$scope.page += 1;
-					localStorage.setItem('page', $scope.page);
-					return $scope.page;
-				};
-
-				// ограничение первой страницы
-				$scope.firstPage = function(){
-					return $scope.page == 1;
-				};
-
-				// ограничение последней страницы
-				$scope.lastPage = function(){
-					return $scope.page == $scope.countPages;
-				};
-
-				// выборка записей запрошенной страницы
-				$scope.start = 	function(){
-					return ($scope.page - 1) * $scope.perPage;
-				}
-
-                angular.forEach($scope.countries, function(country) {
-                    country.Population = parseInt(country.Population);
-                });
+				pagination.setItems(response.data, $scope.perPage);
             });
     }]);
 })();
